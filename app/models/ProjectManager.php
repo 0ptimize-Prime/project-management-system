6482c1fadd2a16722b2af4d8d3b8d2f118bc9c2b
NethumL <nethumpinsara@gmail.com>
2021-09-25 22:57:53 +0530

    Add ProjectManager model

---
+++ app/models/ProjectManager.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once __DIR__ . "/AbstractManager.php";
+require_once __DIR__ . "/../utils.php";
+
+class ProjectManager extends AbstractManager
+{
+    public function createProject(string $manager, string $title, string $description, string $deadline): string
+    {
+        $id = bin2hex(random_bytes(10));
+        $stmt = $this->db->prepare(
+            "INSERT INTO project(id, manager, title, description, deadline) VALUES(?, ?, ?, ?, ?);"
+        );
+        $deadlineTimestamp = convertDateToTimestamp($deadline);
+        $result = $stmt->execute([$id, $manager, $title, $description, $deadlineTimestamp]);
+
+        if ($result) {
+            return $id;
+        } else {
+            die();
+        }
+    }
+
+    public function getProject(string $id): array|false
+    {
+        $stmt = $this->db->prepare("SELECT * FROM project WHERE id=?;");
+        $stmt->execute([$id]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($result) {
+            return $result;
+        } else {
+            return false;
+        }
+    }
+
+    public function updateProject(string $id, string $manager, string $title, string $description, string $deadline): bool
+    {
+        $stmt = $this->db->prepare(
+            "UPDATE project SET manager=?, title=?, description=?, deadline=? WHERE id=?;"
+        );
+        $deadlineTimestamp = convertDateToTimestamp($deadline);
+        return $stmt->execute([$manager, $title, $description, $deadlineTimestamp, $id]);
+    }
+
+    public function deleteProject(string $id): bool
+    {
+        $stmt = $this->db->prepare("DELETE FROM project WHERE id = ?;");
+        return $stmt->execute([$id]);
+    }
+}
