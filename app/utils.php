41a4e975bf7d42d93c1f42cc8ea19eb716815951
Nimantha Cooray <nimanthacooray@gmail.com>
2021-10-02 21:49:25 +0530

    Add flash messages functionality

--- app/utils.php
+++ app/utils.php
@@ -7,3 +7,29 @@ function includeWithVariables($filePath, $variables = [])
         include $filePath;
     }
 }
+
+const FLASH = "FLASH";
+const FLASH_SUCCESS = "success";
+const FLASH_ERROR = "danger";
+
+function create_flash_message(string $name, string $message, string $type) : void {
+    if (isset($_SESSION[FLASH][$name])) {
+        unset($_SESSION[FLASH][$name]);
+    }
+
+    $_SESSION[FLASH][$name] = [
+        "message" => $message,
+        "type" => $type
+    ];
+}
+
+function display_flash_message(string $name) : void {
+    if (!isset($_SESSION[FLASH][$name])) {
+        return;
+    }
+
+    $message_arr = $_SESSION[FLASH][$name];
+    unset($_SESSION[FLASH][$name]);
+    $output = "<div class=\"alert alert-%s\" role=\"alert\">%s</div>";
+    echo sprintf($output, $message_arr["type"], $message_arr["message"]);
+}
