289b504c34daeefa2b46233979ca2752c6bc7855
Nimsara Fernando <nimsara66@gmail.com>
2021-11-18 02:01:38 +0530

    Implement task create function in task controller Implement task create view

--- app/controllers/task.php
+++ app/controllers/task.php
@@ -1,8 +1,62 @@
 <?php
+require_once __DIR__ . "/../utils.php";
+require_once __DIR__."/../models/TaskManager.php";
+require_once __DIR__."/../models/UserManager.php";
+require_once __DIR__."/../models/ProjectManager.php";
 
 class Task extends Controller
 {
     public function create() {
-        $this->view("task/create");
+        session_start();
+        if ($_SESSION["user"]["userType"] == "EMPLOYEE") {
+            header("Location: " . BASE_URL . "home/dashboard");
+            die;
+        }
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            $this->checkAuth("task/create", function () {
+                return ["name" => $_SESSION["user"]["username"]];
+            });
+        } else if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
+            $this->checkAuth("task/create", function () {
+            });
+            $check_user=$this->check_user($_POST["username"]);
+            $check_project=$this->check_project($_POST["projectId"]);
+            if ($check_user && $check_project) {
+                $taskManager = TaskManager::getInstance();
+                $result=$taskManager->addTask(
+                    $_POST["projectId"],
+                    $_POST["title"],
+                    $_POST["description"],
+                    $_POST["username"],
+                    $_POST["deadline"],
+                    $_POST["effort"],
+                );
+                $result=true;
+                if ($result) {
+                    create_flash_message("create-task", "Task `" . $_POST["title"] . "` created successfully.", FLASH_SUCCESS);
+                } else {
+                    create_flash_message("create-task", "Something went wrong, couldn't create task.", FLASH_ERROR);
+                }
+            } else {
+                create_flash_message("create-task", "Invalid request to create task.", FLASH_ERROR);
+            }
+            header("Location: " . BASE_URL . "task/create");
+            die;
+        }
+    }
+
+    private function check_user(string $username):bool 
+    {
+        if(strlen($username) < 1) {
+            return true;
+        }
+        $userManager = UserManager::getInstance();
+        return (bool)$userManager->getUserDetails($username);
+    }
+
+    private function check_project(string $projectId):bool 
+    {
+        $projectManager = ProjectManager::getInstance();
+        return (bool)$projectManager->getProject($projectId);
     }
 }
--- app/views/task/create.php
+++ app/views/task/create.php
@@ -1,1 +1,100 @@
-hello
+<?php require_once __DIR__ . "/../../utils.php" ?>
+
+<html>
+
+<?php includeWithVariables(__DIR__ . "/../templates/header.php",
+    array('title' => 'Create Task',
+        'isLoggedIn' => $_SESSION["user"])) ?>
+<h1>Create Task</h1>
+<?php display_flash_message("create-task") ?>
+<form action="create" method="post">
+    <div class="form-group row">
+        <label for="projectId" class="col-sm-2 col-form-label">Project ID</label>
+        <div class="col-sm-10">
+            <input
+                    class="form-control"
+                    id="projectId"
+                    name="projectId"
+                    placeholder="enigma"
+                    required
+            >
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="title" class="col-sm-2 col-form-label">Title</label>
+        <div class="col-sm-10">
+            <input
+                    class="form-control"
+                    id="title"
+                    name="title"
+                    placeholder="Title"
+                    required
+            >
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="description" class="col-sm-2 col-form-label">Description</label>
+        <div class="col-sm-10">
+            <input
+                    class="form-control"
+                    type="text"
+                    id="description"
+                    name="description"
+                    placeholder="Description"
+            >
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="username" class="col-sm-2 col-form-label">Username</label>
+        <div class="col-sm-10">
+            <input
+                    class="form-control"
+                    id="username"
+                    name="username"
+                    placeholder="chuck66"
+            >
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="deadline" class="col-sm-2 col-form-label">Deadline</label>
+        <div class="col-sm-10">
+            <input
+                    class="form-control"
+                    type="date"
+                    id="deadline"
+                    name="deadline"
+            >
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="effort" class="col-sm-2 col-form-label">Effort</label>
+        <div class="col-sm-10">
+            <input
+                    type="number"
+                    class="form-control"
+                    id="effort"
+                    name="effort"
+                    placeholder="Effort"
+                    min="0"
+                    max="11"
+                    required
+            >
+        </div>
+    </div>
+    <div class="form-group row">
+        <div class="col-sm-10">
+            <button
+                    type="submit"
+                    class="btn btn-primary"
+                    name="submit"
+                    value="submit"
+            >
+                Submit
+            </button>
+        </div>
+    </div>
+</form>
+
+</body>
+
+</html>
