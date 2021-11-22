d8f443b3803b4f6b9715d3cfb44ff88423b1d053
NethumL <nethumpinsara@gmail.com>
2021-11-22 18:30:43 +0530

    Redesign login page

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -14,51 +14,25 @@ $data = $data ?? [];
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="<?php echo BASE_URL?>css/style.css "/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Calisto MT"/>
-    <style>
-        body {
-            font-family: "Calisto MT", sans-serif;
-
-        }
-    </style>
-    <title>Bootstrap 5 Login form for project</title>
+    <link rel="stylesheet" href="<?php echo BASE_URL?>css/login.css "/>
 </head>
 <body>
-<div class="global-container">
-    <div class="card login-form">
-        <div class="card-body">
-            <h1 class="card-title text-center">LOGIN</h1>
-            <div class="card-text">
-                <form>
-                    <div class="form-group">
-                        <label for="Username">Username</label>
-                        <input
-                                type="text"
-                                name="username"
-                                id="username"
-                                class="form-control"
-                                aria-describedby="usernameHelp"
-                        />
-                    </div>
-
-                    <div class="form-group">
-                        <label for="Password">Password</label>
-                        <input
-                                type="password"
-                                name="password"
-                                id="password"
-                                class="form-control"
-                                aria-describedby="passwordHelp"
-                        />
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-block"> Login </button>
-                </form>
-            </div>
+<?php display_flash_message("login") ?>
+<div class="login-form">
+    <form action="login" method="post">
+        <h2 class="text-center">Login</h2>
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" name="username" id="username" placeholder="Username" required="required">
+            <label for="username">Username</label>
         </div>
-    </div>
+        <div class="form-floating mb-3">
+            <input type="password" class="form-control" name="password" id="password" placeholder="Password" required="required">
+            <label for="password">Password</label>
+        </div>
+        <div class="form-group d-grid m-2">
+            <button type="submit" class="btn btn-primary">Log in</button>
+        </div>
+    </form>
 </div>
 
 </body>
