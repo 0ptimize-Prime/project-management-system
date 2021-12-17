f13b95d5a822f7121389c76242f9c1f3ff215b96
Nimantha Cooray <nimanthacooray@gmail.com>
2021-12-17 14:51:07 +0530

    Implement Strategy design pattern for flash msgs

--- app/utils.php
+++ app/utils.php
@@ -19,24 +19,67 @@ const FLASH = "FLASH";
 const FLASH_SUCCESS = "success";
 const FLASH_ERROR = "danger";
 
-function create_flash_message(string $name, string $message, string $type) : void {
-    if (isset($_SESSION[FLASH][$name])) {
-        unset($_SESSION[FLASH][$name]);
+class FlashMessage
+{
+    private FlashMessageType $type;
+    private string $name, $msg;
+
+    private function __construct(string $name, string $msg, FlashMessageType $type)
+    {
+        $this->type = $type;
+        $this->name = $name;
+        $this->msg = $msg;
     }
 
-    $_SESSION[FLASH][$name] = [
-        "message" => $message,
-        "type" => $type
-    ];
+    public static function create_flash_message(string $name, string $message, FlashMessageType $type): void
+    {
+        $new_msg = new FlashMessage($name, $message, $type);
+        if (isset($_SESSION[FLASH][$name])) {
+            unset($_SESSION[FLASH][$name]);
+        }
+
+        $_SESSION[FLASH][$name] = serialize($new_msg);
+    }
+
+    public function get_message(): string
+    {
+        return $this->msg;
+    }
+
+    public function get_flash_message_type(): FlashMessageType
+    {
+        return $this->type;
+    }
+
+    public static function display_flash_message(string $name): void
+    {
+        if (isset($_SESSION[FLASH][$name])) {
+            $msg_obj = unserialize($_SESSION[FLASH][$name]);
+            $msg_obj->get_flash_message_type()->display($msg_obj->get_message());
+            unset($_SESSION[FLASH][$name]);
+        }
+    }
 }
 
-function display_flash_message(string $name) : void {
-    if (!isset($_SESSION[FLASH][$name])) {
-        return;
-    }
+interface FlashMessageType
+{
+    public function display(string $message);
+}
 
-    $message_arr = $_SESSION[FLASH][$name];
-    unset($_SESSION[FLASH][$name]);
-    $output = "<div class=\"alert alert-%s\" role=\"alert\">%s</div>";
-    echo sprintf($output, $message_arr["type"], htmlspecialchars($message_arr["message"]));
+class SuccessFlashMessage implements FlashMessageType
+{
+    public function display(string $message): void
+    {
+        $output = "<div class=\"alert alert-success flash-message\" style='top: 55px' role=\"alert\">%s</div>";
+        echo sprintf($output, htmlspecialchars($message));
+    }
+}
+
+class ErrorFlashMessage implements FlashMessageType
+{
+    public function display(string $message): void
+    {
+        $output = "<div class=\"alert alert-danger flash-message\" style='top: 55px' role=\"alert\">%s</div>";
+        echo sprintf($output, htmlspecialchars($message));
+    }
 }
