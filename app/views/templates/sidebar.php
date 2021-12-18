bb772370d4918936ce2eef6e02eb587d18343909
NethumL <nethumpinsara@gmail.com>
2021-12-18 18:20:27 +0530

    Remove unnecessary CSS in sidebar

--- app/views/templates/sidebar.php
+++ app/views/templates/sidebar.php
@@ -2,7 +2,7 @@
 $isAdmin = $isAdmin ?? false;
 ?>
 
-<nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
+<nav id="sidebarMenu" class="d-lg-block sidebar collapse bg-white">
     <div class="position-sticky">
         <div class="list-group list-group-flush mx-3 mt-4">
             <?php if ($isAdmin) { ?>
