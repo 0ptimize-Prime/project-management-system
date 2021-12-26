9e8e5f5c43358edef05bac1ccd8e52fbea43f85b
NethumL <nethumpinsara@gmail.com>
2021-12-26 16:52:34 +0530

    Implement user search in admin/edit view

--- public/js/admin-edit.js
+++ public/js/admin-edit.js
@@ -14,3 +14,41 @@ const removeButton = document.getElementById("remove-button");
 const imgInput = document.getElementById("profile_picture");
 const preview = document.getElementById("preview");
 const removeDpButton = document.getElementById("remove-dp-button");
+
+searchForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const queryParts = Array.from(searchFormFields).map(field => {
+        return field.getAttribute("name") + "=" + encodeURIComponent(field.value);
+    });
+    const query = "?" + queryParts.join("&");
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.withCredentials = true;
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = JSON.parse(this.response);
+            const tbody = table.querySelector("tbody");
+            tbody.innerHTML = "";
+            response.forEach(user => {
+                const tr = document.createElement("tr");
+                const profileTd = document.createElement("td");
+                profileTd.textContent = user.profile_picture;
+                profileTd.style = "display: none;";
+                tr.appendChild(profileTd);
+                const usernameTd = document.createElement("td");
+                usernameTd.textContent = user.username;
+                tr.appendChild(usernameTd);
+                const nameTd = document.createElement("td");
+                nameTd.textContent = user.name
+                tr.appendChild(nameTd);
+                const userTypeTd = document.createElement("td");
+                userTypeTd.textContent = user.userType;
+                tr.appendChild(userTypeTd);
+                tbody.appendChild(tr);
+            });
+        }
+    }
+    xhttp.open("GET", BASE_URL + "admin/search" + query, true);
+    xhttp.send();
+});
