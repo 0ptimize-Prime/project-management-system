37d0a6f62369f8f1e6ea369bcddfbed93d1d2830
NethumL <nethumpinsara@gmail.com>
2021-12-26 16:42:07 +0530

    Store `BASE_URL` in head

--- app/views/templates/head.php
+++ app/views/templates/head.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="BASE_URL" content="<?php echo htmlspecialchars(BASE_URL) ?>">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
