755be056cfcd548f16510996221499b7aa1f422b
Pamudu Wijekoon <pamudu.palinda@gmail.com>
2022-01-04 10:30:42 +0530

    remove comments on project/view

--- app/views/project/view.php
+++ app/views/project/view.php
@@ -103,16 +103,10 @@ includeWithVariables(__DIR__ . "/../templates/navbar.php", array("isLoggedIn" =>
     </div>
 </div>
 <script>
-    //Get the button:
     mybutton = document.getElementById("myBtn");
-
-    // When the user scrolls down 20px from the top of the document, show the button
-
-
-    // When the user clicks on the button, scroll to the top of the document
     function topFunction() {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
     }
 </script>
 
