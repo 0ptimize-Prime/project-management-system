871a9aa70a76c413c0ee18e16c005ed1a326cb0e
Nimantha Cooray <nimanthacooray@gmail.com>
2022-01-08 18:05:22 +0530

    Add `milestone/create` endpoint

--- app/controllers/milestone.php
+++ app/controllers/milestone.php
@@ -1,9 +1,40 @@
 <?php
 require_once __DIR__ . "/../models/MilestoneManager.php";
+require_once __DIR__ . "/../models/ProjectManager.php";
 
 class milestone extends Controller
 {
-    public function create() {}
+    public function create()
+    {
+        $milestoneManager = MilestoneManager::getInstance();
+        $projectManager = ProjectManager::getInstance();
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $this->checkAuth("milestone/create", function (){});
+        }
 
-    public function remove() {}
+        if (!isset(
+            $_POST["id"],
+            $_POST["title"]
+            ) ||
+            empty($_POST["id"]) ||
+            empty($_POST["title"])
+        ) {
+            http_response_code(400);
+            die;
+        }
+
+        $project = $projectManager->getProject($_POST["id"]);
+        if (!$project || $project["manager"] !== $_SESSION["user"]["username"]) {
+            http_response_code(400);
+            die;
+        }
+
+        $result = $milestoneManager->addMilestone($_POST["id"], $_POST["title"]);
+        if (!$result)
+            http_response_code(400);
+    }
+
+    public function remove()
+    {
+    }
 }
