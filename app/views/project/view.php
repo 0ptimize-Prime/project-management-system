185b538810e463cbab9f60ae4b2c14c49f710166
NethumL <nethumpinsara@gmail.com>
2022-01-08 22:32:45 +0530

    Style project details section in project/view

--- app/views/project/view.php
+++ app/views/project/view.php
@@ -6,7 +6,6 @@
 <head>
     <title>Project</title>
     <?php include __DIR__ . "/../templates/head.php" ?>
-    <link rel="stylesheet" href="<?php echo BASE_URL ?>css/project-view.css?<?php echo time(); ?>"/>
     <style>
         .card-text {
             white-space: pre-wrap;
@@ -16,11 +15,11 @@
             margin: auto;
             width: 50%;
         }
+
         .table th {
             background-color: dodgerblue;
             color: white;
         }
-
     </style>
 </head>
 
@@ -32,30 +31,53 @@ showNavbar($data);
 
 <h1 class="text-center">Project view</h1>
 
-<div class="d-flex">
-    <div class="card mx-auto" style="width: 18rem;">
+<div class="d-flex pt-3">
+    <div class="card mx-auto" style="width: 40rem;">
         <div class="card-body">
-            <h5 class="card-title">Project: <?php echo htmlspecialchars($data["project"]["title"]) ?></h5>
-            <p class="card-text">Description: <?php echo htmlspecialchars($data["project"]["description"]) ?></p>
-            <h6 class="card-subtitle mb-2 text-muted">
-                Manager: <?php echo htmlspecialchars($data["project"]["manager"]) ?></h6>
-            <h6 class="card-subtitle mb-2 text-muted">
-                Created at : <?php echo htmlspecialchars($data['project']['created_at']) ?></h6>
-            <h6 class="card-subtitle mb-2 text-muted">
-                Deadline : <?php echo htmlspecialchars($data['project']['deadline']) ?></h6>
-            <h6 class="card-subtitle mb-2 text-muted">
-                Status: <?php echo htmlspecialchars($data["project"]["status"]) ?></h6>
-            <ul class="list-group list-group-flush">
-                <?php foreach ($data["files"] as $file) { ?>
-                    <h6 class="card-subtitle mb-2 text-muted">
-                        Files:</h6>
-                    <li class="list-group-item">
-                        <a href="<?php echo BASE_URL . "uploads/" . htmlspecialchars($file["id"]) ?>">
-                            <?php echo htmlspecialchars($file["name"]) ?>
-                        </a>
-                    </li>
-                <?php } ?>
-            </ul>
+            <h5 class="card-title text-center">Project: <?php echo htmlspecialchars($data["project"]["title"]) ?></h5>
+            <div class="px-3 pt-2">
+                <div class="row mb-3">
+                    <div class="col-3">Description:</div>
+                    <div class="col-9 card-text"><?php echo htmlspecialchars($data["project"]["description"]) ?></div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-3">Manager:</div>
+                    <div class="col-4"><?php echo htmlspecialchars($data["project"]["managerName"]) ?></div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-3">Created at:</div>
+                    <div class="col-4"><?php echo htmlspecialchars($data["project"]["created_at"]) ?></div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-3">Deadline:</div>
+                    <div class="col-4">
+                        <?php echo htmlspecialchars($data["project"]["deadline"] ?? "None") ?>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-3">Status:</div>
+                    <div class="col-2">
+                        <span class="badge rounded-pill bg-primary">
+                            <?php echo htmlspecialchars($data["project"]["status"]) ?>
+                        </span>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-3">Files:</div>
+                    <?php if (count($data["files"]) == 0) { ?>
+                        <div class="col-9">No files</div>
+                    <?php } ?>
+                    <ul class="list-group col-9">
+                        <?php foreach ($data["files"] as $file) { ?>
+                            <li class="list-group-item">
+                                <a href="<?php echo htmlspecialchars(BASE_URL . 'uploads/' . $file['id']) ?>">
+                                    <?php echo htmlspecialchars($file["name"]) ?>
+                                </a>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </div>
