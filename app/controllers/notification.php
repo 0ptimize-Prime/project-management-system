c42b2ad3c0f8057f77715e70a801a1a2b32c45b2
Nimantha Cooray <nimanthacooray@gmail.com>
2022-01-09 14:58:58 +0530

    Implement `notification/status` endpoint

--- app/controllers/notification.php
+++ app/controllers/notification.php
@@ -4,11 +4,20 @@ require_once __DIR__ . "/../models/NotificationManager.php";
 
 class notification extends Controller
 {
-    public function status($id = null)
+    public function status(string $id = null)
     {
         if ($_SERVER["REQUEST_METHOD"] === "PUT") {
-            $this->checkAuth("notification/status", function() {});
+            $this->checkAuth("notification/status", function () {
+            });
 
+            $notificationManager = NotificationManager::getInstance();
+            if (!empty($id)) {
+                $result = $notificationManager->markAsRead($id);
+            } else {
+                $result = $notificationManager->markOldAsRead($_SESSION["user"]["username"]);
+            }
+            if (!$result)
+                http_response_code(400);
         }
     }
 }
