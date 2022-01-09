0b6b76e441e38d548ab424fe7452b4fcc7eff81f
Nimantha Cooray <nimanthacooray@gmail.com>
2022-01-09 13:29:46 +0530

    Add `notification/status` endpoint

--- app/controllers/notification.php
+++ app/controllers/notification.php
@@ -4,5 +4,11 @@ require_once __DIR__ . "/../models/NotificationManager.php";
 
 class notification extends Controller
 {
+    public function status($id = null)
+    {
+        if ($_SERVER["REQUEST_METHOD"] === "PUT") {
+            $this->checkAuth("notification/status", function() {});
 
+        }
+    }
 }
