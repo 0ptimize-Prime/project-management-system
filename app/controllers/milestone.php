7563660416a19b68906c3c4319d555daa4d30db3
NethumL <nethumpinsara@gmail.com>
2022-01-21 12:27:37 +0530

    Implement `/milestone/edit` route

--- app/controllers/milestone.php
+++ app/controllers/milestone.php
@@ -37,6 +37,35 @@ class milestone extends Controller
         }
     }
 
+    public function edit()
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $this->checkAuth("milestone/edit", function () {
+            });
+
+            if (!isset($_POST["id"], $_POST["title"]) || empty($_POST["id"]) || empty($_POST["title"])) {
+                http_response_code(400);
+                die;
+            }
+
+            $milestoneManager = MilestoneManager::getInstance();
+            $milestone = $milestoneManager->getMilestone($_POST["id"]);
+            $projectManager = ProjectManager::getInstance();
+            $project = $projectManager->getProject($milestone["project_id"]);
+            if (!$project || $project["manager"] !== $_SESSION["user"]["username"]) {
+                http_response_code(400);
+                die;
+            }
+
+            $result = $milestoneManager->updateMilestone($_POST["id"], $_POST["title"]);
+            if (!$result) {
+                http_response_code(500);
+            } else {
+                echo json_encode($milestoneManager->getMilestone($_POST["id"]));
+            }
+        }
+    }
+
     public function remove($id)
     {
         $milestoneManager = MilestoneManager::getInstance();
